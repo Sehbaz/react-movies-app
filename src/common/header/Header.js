ac3a956c47d5e3bb0e6988b471424459131826f9
Sehbaz Rafik <sehbazm@gmail.com>
2021-03-25 09:55:43 +0000

    Added logo

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./Header.css";
 import Button from "@material-ui/core/Button";
+import TheatersOutlinedIcon from "@material-ui/icons/TheatersOutlined";
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <Button variant="contained">LOGIN</Button>
+      <div className="header-container">
+        <div>
+          <TheatersOutlinedIcon className="logo" />
+        </div>
+        <div className="form">
+          <Button variant="contained" color="default">
+            login
+          </Button>
+        </div>
       </div>
     );
   }
