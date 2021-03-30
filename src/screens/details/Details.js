15b7609bb509d484c6582d82075ad2fe11af07dc
Sehbaz Rafik <sehbazm@gmail.com>
2021-03-30 17:55:33 +0100

    Added ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,6 +3,11 @@ import Header from "../../common/header/Header";
 import moviesData from "../../common/movieData";
 import { Typography } from "@material-ui/core";
 import "../details/Details.css";
+import Home from "../../screens/home/Home";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import YouTube from "react-youtube";
 class Details extends Component {
   constructor() {
     super();
@@ -14,31 +19,81 @@ class Details extends Component {
       return mov.id === this.props.movieId;
     })[0];
     this.setState({ currentState });
-    console.log(currentState);
   }
+  backtohomeHandler = () => {
+    ReactDOM.render(<Home />, document.getElementById("root"));
+  };
   render() {
     let movie = this.state.movie;
-    console.log(movie.poster_url);
+    const opts = {
+      playerVars: {
+        autoplay: 1,
+      },
+    };
     return (
       <div className="details">
         <Header />
+        <div className="back">
+          <Button
+            variant="outlined"
+            color="default"
+            startIcon={<ArrowBackIcon />}
+          >
+            <Typography onClick={this.backtohomeHandler}>
+              Back to home
+            </Typography>
+          </Button>
+        </div>
         <div className="flex-containerDetails">
           <div className="leftDetails">
             <img src={movie.poster_url} alt={movie.title}></img>
           </div>
           <div className="middleDetails">
+            <div className="movie-title">{movie.title}</div>
             <div>
               <Typography>
                 <span className="bold">Genre : </span>
                 {movie.genres.join(",")}
               </Typography>
+            </div>
+            <div>
               <Typography>
-                <span className="bold">Title : </span>
-                {movie.title}
+                <span className="bold">Duration : </span>
+                {movie.duration}
+                <span>mins</span>
               </Typography>
             </div>
+            <div>
+              <Typography>
+                <span className="bold">Rating : </span>
+                <span>" {movie.censor_board_rating}"</span>
+              </Typography>
+            </div>
+            <div>
+              <Typography>
+                <span className="bold">Release Date : </span>
+                <span>{new Date(movie.release_date).toDateString()}</span>
+              </Typography>
+            </div>
+            <br />
+
+            <div>
+              <Typography>
+                <span className="bold">Story Line : </span>
+                <span>{movie.storyline}</span>
+              </Typography>
+            </div>
+            <br />
+            <div className="trailer-container">
+              <span className="bold">Trailer : </span>
+              <YouTube
+                videoId={movie.trailer_url.split("?v=")[1]}
+                opts={opts}
+                onReady={this._onReady}
+              ></YouTube>
+            </div>
           </div>
-          <div className="rightDetails">hi</div>
+          <div className="rightDetails"></div>
         </div>
       </div>
     );
