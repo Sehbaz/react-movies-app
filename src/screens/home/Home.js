f35de2805bf6b9fbc221709cdd4aff5eb47e81d9
Sehbaz Rafik <sehbazm@gmail.com>
2021-03-30 01:17:24 +0100

    Added grid list for ‘Upcoming Movies

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,14 +1,61 @@
 import React, { Component } from "react";
 import "./Home.css";
 import Header from "../../common/header/Header.js";
-
+import { withStyles } from "@material-ui/core/styles";
+import moviesData from "../../common/movieData";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  upcomingMoviesHeading: {
+    textAlign: "center",
+    background: "#e9ecef",
+    padding: "8px",
+    fontSize: "1rem",
+  },
+  gridListUpcomingMovies: {
+    flexWrap: "nowrap",
+    transform: "translateZ(0)",
+    width: "100%",
+  },
+  titleBar: {
+    background:
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+  },
+});
 class Home extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <Header />
+        <div className={classes.upcomingMoviesHeading}>
+          <span>Upcoming Movies</span>
+        </div>
+        <GridList
+          cols={5}
+          cellHeight={300}
+          className={classes.gridListUpcomingMovies}
+        >
+          {moviesData.map((movie) => (
+            <GridListTile key={movie.id}>
+              <img src={movie.poster_url} alt={movie.title}></img>
+              <GridListTileBar
+                title={movie.title}
+                classes={{
+                  root: classes.titleBar,
+                  title: classes.title,
+                }}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
       </div>
     );
   }
 }
-export default Home;
+export default withStyles(styles)(Home);
