498f15b7abe4bce98ae7934cf8a491d14f861e5c
Sehbaz Rafik <sehbazm@gmail.com>
2021-03-30 09:29:32 +0100

    Added 'Movie Name' filter on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,6 +6,10 @@ import moviesData from "../../common/movieData";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import { Typography, TextField } from "@material-ui/core";
 const styles = (theme) => ({
   root: {
     flexGrow: 1,
@@ -22,14 +26,42 @@ const styles = (theme) => ({
     transform: "translateZ(0)",
     width: "100%",
   },
+  gridList: {
+    flexWrap: "nowrap",
+
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: "translateZ(0)",
+  },
+
   titleBar: {
     background:
       "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
   },
+  gridListNewRelease: {
+    cursor: "pointer",
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 240,
+    maxWidth: 240,
+  },
+  title1: {
+    color: theme.palette.primary.light,
+  },
 });
+
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { windowWidth: window.innerWidth, moviesName: "" };
+  }
+
+  movieNameChangeHandler = (event) => {
+    this.setState({ moviesName: event.target.value });
+  };
   render() {
     const { classes } = this.props;
+    let colums = this.state.windowWidth >= 600 ? 5.5 : 2.5;
     return (
       <div>
         <Header />
@@ -37,8 +69,9 @@ class Home extends Component {
           <span>Upcoming Movies</span>
         </div>
         <GridList
-          cols={5}
-          cellHeight={300}
+          className="mygrid"
+          cellHeight={350}
+          cols={colums}
           className={classes.gridListUpcomingMovies}
         >
           {moviesData.map((movie) => (
@@ -47,13 +80,60 @@ class Home extends Component {
               <GridListTileBar
                 title={movie.title}
                 classes={{
-                  root: classes.titleBar,
                   title: classes.title,
                 }}
               />
             </GridListTile>
           ))}
         </GridList>
+        <div className={classes.upcomingMoviesHeading}>
+          <span>New Released Movies</span>
+        </div>
+        <div className="flex-contanier">
+          <div className="left">
+            <GridList
+              className="mygrid"
+              cellHeight={350}
+              cols={4}
+              spacing={20}
+              className={classes.gridListNewRelease}
+            >
+              {moviesData.map((movie) => (
+                <GridListTile key={movie.id}>
+                  <img src={movie.poster_url} alt={movie.title}></img>
+                  <GridListTileBar
+                    title={movie.title}
+                    classes={{
+                      title: classes.title,
+                    }}
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
+          <div className="right">
+            <Card>
+              <CardContent>
+                <FormControl className={classes.formControl}>
+                  <Typography className={classes.title1}>
+                    FIND MOVIES BY :
+                  </Typography>
+                </FormControl>
+                <FormControl className={classes.formControl}>
+                  <Typography className={classes.title1}>
+                    <TextField
+                      id="moviename"
+                      label="moviename "
+                      type="text"
+                      variant="outlined"
+                      onChange={this.movieNameChangeHandler}
+                    />
+                  </Typography>
+                </FormControl>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
     );
   }
